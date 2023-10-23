744666461ca0544284c063b32506f0d218d09a2f
dolabela <brenodolabela@gmail.com>
2023-10-23 20:26:17 -0300

    study: add classes 6-7 of godot alvin roe tutorial

--- Player.gd
+++ Player.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
+var speed = 50
 
-
- 
 
 func _physics_process(delta):
-	position.x += 1
+	var dir = (get_global_mouse_position() -position).normalized()
+	rotation = dir.angle()
+
+	if (Input.is_action_pressed("i_up")):
+		velocity = dir*speed
+
+	move_and_slide()
